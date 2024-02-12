0c4b1c5412212c7c9bcaa00c44a3cbe696fbe498
Julia Barbosa <c0627651@vale.com>
2024-02-12 15:45:47 -0300

    aula 1

---
+++ js/introducao.js
@@ -0,0 +1,60 @@
+//VARIAVEIS
+let  nome = "Aluno";
+let $nomeAluno = "Lucas";
+let _disciplina = "História";
+let nota1 = 10; 
+let notaFinal = "10";
+
+// console.log(nome);
+
+//Tipos de Dados
+let idade = 23;
+let altura = 1.15;
+let estaAutorizado = true;
+let nulo = null;
+let indefinido = undefined;
+
+//NaN - Exemplos
+console.log(0 / 0);
+
+let resultadoFinal = parseInt("Jedir");
+console.log(resultadoFinal);
+
+let raizQuadrada = Math.sqrt(-1);
+console.log(raizQuadrada);
+
+//Operadores
+
+matematicos
+console.log(10 + 20);
+console.log(50 - 20);
+console.log(50 * 20);
+console.log(50 / 20);
+console.log(50 ** 20); //exponenciação;
+console.log(50 % 20); //resto;
+
+//atribuição
+
+let notaAluno1 = 10;
+let notaAluno2 = 5;
+
+notaAluno1 = notaAluno1 + 2;
+
+notaAluno1 += 2;
+notaAluno1 -= 2;
+console.log(notaAluno1);
+console.log(notaAluno1 *= notaAluno2);
+console.log(notaAluno1 /= notaAluno2);
+console.log(notaAluno1 %= notaAluno2);
+
+
+//Comparação
+console.log(notaAluno1 == notaAluno2);
+console.log(notaAluno1 === notaAluno2);
+console.log(notaAluno1 != notaAluno2);
+console.log(notaAluno1 !== notaAluno2);
+console.log(notaAluno1 > notaAluno2);
+console.log(notaAluno1 < notaAluno2);
+console.log(notaAluno1 >= notaAluno2);
+console.log(notaAluno1 <= notaAluno2);
+
